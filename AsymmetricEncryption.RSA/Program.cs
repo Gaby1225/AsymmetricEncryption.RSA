93b41fb1cc5615bf0d7fc1efb443ba08889b0d9d
GABRIELLE NOVAES DA SILVA <082180020@faculdade.cefsa.edu.br>
2023-03-02 20:55:41 -0300

    tentativa 1 de encriptografia

--- AsymmetricEncryption.RSA/Program.cs
+++ AsymmetricEncryption.RSA/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System.Numerics;
+
+public class EncryptMessage
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a mensagem que deseja encriptografar");
+        string messageToEncrypt = Console.ReadLine();
+        string messageEncrypt = "";
+        string messageToDecrypt = "";
+
+        BigInteger x, y, z;
+
+        int bitLength = 4096;
+
+
+    }
+
+}
+
+
+
+
